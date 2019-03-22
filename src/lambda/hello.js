f5c473ae23cfd88e4c5acf2ef0e0e51ca1d77946
Josh Ellis <iamjoshellis@gmail.com>
2019-03-22 15:18:03 +1100

    Update hello.js

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,7 +1,6 @@
 exports.handler = function(event, context, callback) {
-  console.log({event, context, callback});
   callback(null, {
     statusCode: 200,
-    body: "Hello, World"
+    body: JSON.stringify({ event, context });
   });
 };
